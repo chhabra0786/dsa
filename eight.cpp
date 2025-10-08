37f0053bed123e6a50f70c763b7076c13c91d7b8
Ritish Chhabra <ritishchhabra0786@gmail.com>
2025-10-08 13:03:54 +0530

    Initial commit

---
+++ eight.cpp
@@ -0,0 +1,82 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int num=2;
+//     cout<<endl;
+//     switch(num){
+//         case 1: cout<<"First" <<endl;
+//         break;
+//          case 2: cout<<"Second"<<endl;
+//          break;
+//     }
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter vlaue of a is :";
+//     cin>>a;
+    
+//     int b;
+//     cout<<"Enter vlaue of b is :";
+//     cin>>b;
+    
+//     char op;
+//     cout<<"Enter operrator :";
+//     cin>>op;
+
+//    switch (op){
+//     case '+': cout<<(a+b)<<endl;
+//     break;
+//     case '-': cout<<(a-b);
+//     break;
+//     case '/': cout<<(a/b);
+//     break;
+//     case '%': cout<<(a%b);
+//     break;
+//     case '*': cout<<(a*b);
+// break;
+
+//    }
+//    return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int ruppees;
+//     int count;
+//     cout<<"Enter ruppees";
+//     cin>>ruppees;
+//     switch (count){
+//         case 1 : cout<<( int rupees/100);
+//         break;
+//         case 2 : cout<<(int rupees/50);
+//         break;
+
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int power(int num1,int num2){
+//     int a ,b;
+//     cin>>a>>b<<endl;
+//     int ans=1;
+//     for (int i=1;i<=num2;i++){
+// ans=ans*num1;
+// return ans;
+//     }
+// }
+// int main(){
+//     int a ,b ;
+//     cin>>a >>b;
+//     int ans
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    retun 0;
+}
