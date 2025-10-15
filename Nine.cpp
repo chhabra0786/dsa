2650919e50c8a2adf13f38f327fa530bc3fa0ba3
Ritish Chhabra <ritishchhabra0786@gmail.com>
2025-10-15 11:53:41 +0530

    Fixed infinite loop in bitcount function

---
+++ Nine.cpp
@@ -0,0 +1,90 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int dost[15];
+//     cout<<"value of 13th array is :"<<dost[13]<<endl;
+//     cout<<"value of 19th array is :"<<dost[19]<<endl;
+
+
+//     return 0;
+
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=15;
+//     int array[15]={2,3};
+//     for(int i=0;i<=n;i++){
+//         cout<<array[i];
+//     }
+//     return 0;
+    
+// // }
+// //intializing all with zero
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=15;
+//     int array[15]={0};
+//     for(int i=0;i<=n;i++){
+//         cout<<array[i];
+//     }
+//     return 0;
+    
+// }
+//intializing all with one
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=15;
+//     int array[15]={1};
+//     for(int i=0;i<=n;i++){
+//         cout<<array[i];
+//     }
+//     return 0;
+    
+// }
+
+//working arrays with functions
+// #include <iostream>
+// using namespace std;
+// void printArray(int arr[],int size){
+
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i];
+//     }
+//     cout<<endl;
+
+// }
+// int main(){
+//     int first[15]={2,6};
+//     int n=15;
+//     printArray(first,n);
+//     int second[15]={0};
+    
+//     printArray(second,n);
+//     int sizeofArray=size of (arr)/size of (arr[0]);
+
+//     return 0;
+    
+// }
+
+// Maximum and minimum number find in the array 
+
+#include <iostream>
+using namespace std;
+
+void update(int arr[],int n){
+    cout<<"inside the function";
+    for (int i=0;i<3;i++){
+        cout<<arr[i];
+    }
+
+}
+int main(){
+    int arr[3]={1,2,3}
+    return 0;
+    
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,14 @@
-}
+    
+// // }
+// //intializing all with zero
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=15;
+//     int array[15]={0};
+//     for(int i=0;i<=n;i++){
+//         cout<<array[i];
+//     }
+//     return 0;
+    
+// }
