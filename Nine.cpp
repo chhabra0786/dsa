d66f2fcabc0bb22076b27a7ccdf935d26c943ce2
Ritish Chhabra <ritishchhabra0786@gmail.com>
2025-10-27 13:03:18 +0530

    Fixed infinite loop in bitcount function

--- Nine.cpp
+++ Nine.cpp
@@ -71,20 +71,60 @@
     
 // }
 
-// Maximum and minimum number find in the array 
+//Linear search
+ 
+// #include <iostream>
+// using namespace std;
+// bool search(int arr[],int size,int key){
+//     for(int i=0;i<size;i++){
+//         if (arr[i]==key){
+//         return 1;
+//     }
+//     }
+//     return 0;
 
-#include <iostream>
-using namespace std;
+// }
+// int main(){
+//     int arr[6]={63,78,86,68};
+//     int key;
+//     cout<<"Enter Element:";
+//     cin>>key;
+//     bool found=search(arr,6,key);
+//     if (found){
+//         cout<<"Element is present";
+//     }
+//     else {
+//         cout<<"Element is absent";
+//     }
 
-void update(int arr[],int n){
-    cout<<"inside the function";
-    for (int i=0;i<3;i++){
-        cout<<arr[i];
-    }
+//     return 0;
 
-}
-int main(){
-    int arr[3]={1,2,3}
-    return 0;
-    
-}
+// }
+
+//Reverse the element 
+
+// #include <iostream>
+// using namespace std;
+// void Reverse(int arr[],int n){
+//     int start=0;
+//     int end=n-1;
+//     while(start<=end){
+//         swap(arr[start],arr[end]);
+//         start++;
+//         end--;
+//     }
+      
+//       }
+
+// void printArray(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<""<<endl;
+// }
+// int main(){
+//     int arr[6]={1,2,3,4,5,6};
+//     Reverse(arr,6);
+//     printArray(arr,6);
+//     return 0;
+// }
