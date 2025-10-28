d71494505b990fda7a5e86150cfcdf6ab2ac19f2
Ritish Chhabra <ritishchhabra0786@gmail.com>
2025-10-28 22:01:49 +0530

    Fixed infinite loop in bitcount function

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,6 @@
-    
-// // }
-// //intializing all with zero
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n=15;
-//     int array[15]={0};
-//     for(int i=0;i<=n;i++){
-//         cout<<array[i];
-//     }
-//     return 0;
-    
-// }
+void swapAlternate(int arr[],int size){
+    for(int i=0;i<size;i+=2){
+        swap (arr[i],arr[i+1]);
+    }
+
+}
---
+++ tenth.cpp
@@ -0,0 +1,35 @@
+// //swapping Alternate numbers
+// #include <iostream>
+// using namespace std;
+// void printArray(int arr[],int size){
+// for(int i=0;i<size;i++){
+//     cout<<arr[i]<<" ";
+// }
+// }
+
+// void swapAlternate(int arr[],int size){
+//     for(int i=0;i<size;i+=2){
+//         swap (arr[i],arr[i+1]);
+//     }
+
+// }
+// int main(){
+//     int even[8]={0,1,2,3,4,5,6,47};
+//     swapAlternate(even,8);
+
+//     printArray(even,8);
+
+//     return 0;
+// }
+// i having related 2 elements same then it can be cancelled to each other and 1 element that is different we can write only those number 
+//like {22,9,9,66,5,5} in this 5 is different that we printed in it.
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[];
+//     cout<<"Enter array elemts:"<<arr[]<<endl;
+
+//     return 0;
+// }
