b5a8c871ab0c44d9c9024d910c0bcde508461190
Ritish Chhabra <ritishchhabra0786@gmail.com>
2025-11-13 13:17:19 +0530

    Fixed infinite loop in bitcount function

---
+++ thirteen.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int leftOccurence(int arr[],int key,int size){
+   int s=0;int e=n-1;
+   int mid=(s+e)/2;
+   int ans=-1;
+   while(s<=e){
+    if(mid==key){
+
+    }
+    else if(mid){
+
+    }
+   }
+}
+int main(){
+    int even[6]={1,2,2,3,4};
+    int odd[5]={1,3,3,4,5}
+    return 0;
+}
+
+//Peak mountain value
+
+#include <iostream>
+using namespace std;
+int s=0;
+int e=arr.size-1;
+int mid=s+(e-s)/2
+int main(){
+    int arr[]={}
+}
